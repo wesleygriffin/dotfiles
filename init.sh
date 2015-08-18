a4b8c3ccc8262d4a4111efea57d07aca16618c21
Wesley Griffin <wgriffin@fraktured.net>
2015-08-18 00:17:53 -0400

    start init script

---
+++ init.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if [[ $EUID -eq 0 ]]; then
+    if [[ -z $1 ]]; then
+        echo "Running this script as root requires a username to be provided"
+        exit 1
+    fi
+    chown_user=$1
+    shift
+fi
+
