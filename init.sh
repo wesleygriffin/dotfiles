f4bdce75c56694eed8b76cf0e553c51839b54694
Wesley Griffin <wesley.griffin@nist.gov>
2015-09-12 15:42:03 -0400

    cleanup

--- init.sh
+++ init.sh
@@ -1,45 +1,54 @@
 #!/bin/bash
 
+file_dir=$(readlink -f ${BASH_SOURCE%/*})
+
 if [[ $EUID -eq 0 ]]; then
     if [[ -z $1 ]]; then
-        echo "Running this script as root requires a username to be provided"
+        echo "$0: running as root requires a username to be provided"
         exit 1
     fi
     chown_user=$1
     shift
 fi
 
-file_dir=$(readlink -f ${BASH_SOURCE%/*})
-
-cd $HOME
-#echo "Installing files into $PWD"
+if [[ -z $1 ]]; then
+    cd $HOME
+elif [[ ! -d $1 ]]; then
+    echo "$0: $1 is not a directory; exiting"
+    exit 1
+else
+    cd $1
+fi
 
 for target in $file_dir/files/.*; do
-    if [ $(basename $target) == "." -o $(basename $target) == ".." ]; then continue; fi
-    ln -s $target $(basename $target)
+    link_name=$(basename $target)
+    if [[ $link_name == "." || $link_name == ".." ]]; then continue; fi
+
+    ln -sf $target $link_name
+
+    if [[ ! -z $chown_user ]]; then chown $chown_user $target; fi
 done
 
 for target in $file_dir/files/*; do
-    split=$(echo $(basename $target) | cut -f1 -d-)
-    if [ $split == "split" ]; then
-        split=$(echo $target | cut -f2 -d-)
-        dir=$(echo $(basename $split) | cut -f1 -d_)
-        file=$(echo $split | cut -f2 -d_)
-        ln -s $target .$dir/$file
-    else
-        ln -s $target $(basename $target)
-    fi
+    link_name=$(basename $target)
+    ln -sf $target $link_name
+
+    if [[ ! -z $chown_user ]]; then chown $chown_user $target; fi
 done
 
-if [ ! -d bin ]; then mkdir bin; fi
+if [[ ! -d bin ]]; then mkdir bin; fi
 for target in $file_dir/bin/*; do
-    ln -s $target bin/$(basename $target)
+    link_name=$(basename $target)
+    ln -sf $target bin/$link_name
+
+    if [[ ! -z $chown_user ]]; then chown $chown_user $target; fi
 done
 
-if [ ! -d .fonts ]; then mkdir .fonts; fi
+if [[ ! -d .fonts ]]; then mkdir .fonts; fi
 for target in $file_dir/fonts/*; do
-    ln -s $target .fonts/$(basename $target)
+    link_name=$(basename $target)
+    ln -sf $target .fonts/$link_name
+
+    if [[ ! -z $chown_user ]]; then chown $chown_user $target; fi
 done
 
-#- .bash_profile
-#- .bashrc
