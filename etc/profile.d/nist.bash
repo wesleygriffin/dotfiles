42abf13a4ebe7ed6f85be372b3078391bd03b3c4
Wesley Griffin <wesley.griffin@nist.gov>
2016-11-10 12:02:18 -0500

    add clang_complete submodule

--- etc/profile.d/nist.bash
+++ etc/profile.d/nist.bash
@@ -1,11 +1,13 @@
-alias sudo='echo NO!'
-alias testing='echo NO!'
+if [[ ${HOSTNAME} =~ *.nist.gov ]]; then
+    alias sudo='echo NO!'
+    alias testing='echo NO!'
 
-alias dtkdebug="export DTK_SPEW_LEVEL=DEBUG"     # Max spewage
-alias dtkinfo="export DTK_SPEW_LEVEL=INFO"
-alias dtknotice="export DTK_SPEW_LEVEL=NOTICE"
-alias dtkwarn="export DTK_SPEW_LEVEL=WARN"
-alias dtkerror="export DTK_SPEW_LEVEL=ERROR"
-alias dtkfatal="export DTK_SPEW_LEVEL=FATAL"      # Min spewage
-alias dtkquiet="unset DTK_SPEW_LEVEL"
+    alias dtkdebug="export DTK_SPEW_LEVEL=DEBUG"     # Max spewage
+    alias dtkinfo="export DTK_SPEW_LEVEL=INFO"
+    alias dtknotice="export DTK_SPEW_LEVEL=NOTICE"
+    alias dtkwarn="export DTK_SPEW_LEVEL=WARN"
+    alias dtkerror="export DTK_SPEW_LEVEL=ERROR"
+    alias dtkfatal="export DTK_SPEW_LEVEL=FATAL"      # Min spewage
+    alias dtkquiet="unset DTK_SPEW_LEVEL"
+fi
 
